d4a98263f8cb86747fed9fdb8bd66c496f46f72f
Demis Bellot <demis.bellot@gmail.com>
2024-11-01 20:20:27 +0800

    export necessary types for drivers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-import { Connection, ConnectionBase, SyncConnection } from "./connection"
+import { Connection, ConnectionBase, NamingStrategy, SyncConnection } from "./connection"
 import { WhereQuery } from "./builders/where"
 import { SelectQuery } from "./builders/select"
 import { DeleteQuery } from "./builders/delete"
 import { Sql } from "./query"
 import { Inspect } from "./inspect"
+import { converterFor, DateTimeConverter } from "./converters"
+import { DataType, DefaultValues } from "./model"
 
 export { 
   Sql,
   ConnectionBase,
   Connection,
   SyncConnection,
+  NamingStrategy,
   WhereQuery,
   SelectQuery,
   DeleteQuery,
   Inspect,
+  converterFor,
+  DateTimeConverter,
+  DataType, 
+  DefaultValues,
 }
